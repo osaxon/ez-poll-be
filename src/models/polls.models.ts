a15adbe2fb737a032217ec96a633d47a1dff8108
Oliver Saxon <oliverrsaxon@gmail.com>
2023-12-13 15:26:38 +0000

    adds websocket server connection events

--- src/models/polls.models.ts
+++ src/models/polls.models.ts
@@ -49,3 +49,30 @@ export const selectUsersPolls = async (
     return error;
   }
 };
+
+export const selectSinglePoll = async (pollId: string) => {
+  console.log(pollId < '<--- the poll id');
+  try {
+    const result = await db
+      .select({
+        id: poll.id,
+        question: poll.question,
+        userId: poll.userId,
+        createdAt: poll.createdAt,
+        options: sql<
+          string[]
+        >`COALESCE(jsonb_agg(${pollOption.text}), '[]'::jsonb) AS options`,
+      })
+      .from(poll)
+      .leftJoin(pollOption, eq(poll.id, pollOption.pollId))
+      .where(eq(poll.id, +pollId))
+      .groupBy(poll.id, poll.question);
+    return {
+      ...result[0],
+      options: result[0].options.map((opt) => ({ text: opt, votes: [] })),
+    };
+  } catch (error) {
+    console.error(error, '<---- db error');
+    return error;
+  }
+};
